cec7a855e1810b34456689677fbc024c49b47619
dlagos78 <lagos.d@gmail.com>
2019-01-11 16:19:14 -0800

    Add files via upload

--- ruby/txt_based_game.rb
+++ ruby/txt_based_game.rb
@@ -4,13 +4,20 @@ puts ''
 puts "(A) hit the snooze button and stay in bed"
 puts "(B) get up and go to school"
 
-start_choice = gets.chomp.upcase
 
-def start start_choice
-  if start_choice == "A" || start_choice.include?("BED")
-  playing_sick
-elsif start_choice == "B" || start_choice.include?("SCHOOL")
-    puts "Wow, lame. Game over!"
+def start
+  start_choice = ""
+  while
+    start_choice = gets.chomp.upcase
+    if start_choice == "A" || start_choice.include?("BED")
+    playing_sick
+    break
+    elsif start_choice == "B" || start_choice.include?("SCHOOL")
+      puts "Wow, lame. Game over!"
+      break
+    else
+      puts "Input valid phrase"
+    end
   end
 end
 
@@ -19,11 +26,17 @@ def playing_sick
   puts
   puts "(A) pretend to be sick"
   puts "(B) talk back to her"
+  while
   sick_choice = gets.chomp.upcase
   if sick_choice == "A" || sick_choice.include?("SICK")
     are_sick
+      break
   elsif sick_choice == "B" || sick_choice.include?("TALK")
     talk_back
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -32,11 +45,17 @@ def are_sick
   puts
   puts "(A) call Cameron to see if he wants to skip school too"
   puts "(B) do your own thing for the day"
+  while
   are_sick_choice = gets.chomp.upcase
   if are_sick_choice == "A" || are_sick_choice.include?("CAMERON")
     call_cameron
+    break
   elsif are_sick_choice == "B" || are_sick_choice.include?("THING")
     go_alone
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -45,11 +64,17 @@ def talk_back
   puts
   puts "(A) pretend to be sick and go to the nurses office"
   puts "(B) keep walking, go out the back door and leave"
+  while
   talk_back_choice = gets.chomp.upcase
   if talk_back_choice == "A" || talk_back_choice.include?("NURSE")
     nurses_office
+    break
   elsif talk_back_choice == "B" || talk_back_choice.include?("LEAVE")
     disappear_campus
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -58,12 +83,18 @@ def nurses_office
   puts
   puts "(A) try to fake a cough"
   puts "(B) fake a stomach ache"
+  while
   nurses_office_choice = gets.chomp.upcase
   if nurses_office_choice == "A" || nurses_office_choice.include?("COUGH")
     puts "The nurse doesn't buy it. She sends you to class, and you spend the rest of the day at school. Game Over!"
+    break
   elsif nurses_office_choice == "B" || nurses_office_choice.include?("STOMACH")
     puts "The nurse buys it."
     disappear_campus
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -72,11 +103,17 @@ def disappear_campus
   puts
   puts "(A) call Cameron to see if he wants to skip school too"
   puts "(B) do your own thing for the day"
+  while
   disappear_campus_choice = gets.chomp.upcase
   if disappear_campus_choice == "A" || disappear_campus_choice.include?("CAMERON")
     call_cameron
+    break
   elsif disappear_campus_choice == "B" || disappear_campus_choice.include?("THING")
     go_alone
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -85,11 +122,17 @@ def call_cameron
   puts
   puts "(A) go over to his house anyway to change his mind"
   puts "(B) do your own thing for the day"
+  while
   call_cameron_choice = gets.chomp.upcase
   if call_cameron_choice == "A" || call_cameron_choice.include?("HOUSE")
     convince_cameron
+    break
   elsif call_cameron_choice == "B" || call_cameron_choice.include?("THING")
     go_alone
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -98,11 +141,17 @@ def convince_cameron
   puts
   puts "(A) Grab the keys to his dad's Ferrari"
   puts "(B) Leave"
+  while
   convince_cameron_choice = gets.chomp.upcase
   if convince_cameron_choice == "A" || convince_cameron_choice.include?("FERRARI")
     ferrari_choice
+    break
   elsif convince_cameron_choice == "B" || convince_cameron_choice.include?("LEAVE")
     go_alone
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -111,12 +160,18 @@ def ferrari_choice
   puts
   puts "(A) Stay with Cameron like a good friend"
   puts "(B) Run away"
+  while
   ferrari_choice = gets.chomp.upcase
   if ferrari_choice == "A" || ferrari_choice.include?("STAY")
     puts "Cameron's dad comes home and sees what you've done. You get in a lot of trouble. Game over!"
+    break
   elsif ferrari_choice == "B" || ferrari_choice.include?("RUN") || ferrari_choice.include?("LEAVE")
     puts "Cold blooded..."
     go_alone
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
@@ -127,16 +182,24 @@ def go_alone
   puts "(B) Go to the art museum"
   puts "(C) Go to a fancy restaurant"
   puts "(D) Go to Sears Tower"
+  while
   go_alone_choice = gets.chomp.upcase
   if go_alone_choice == "A" || go_alone_choice.include?("PARADE") || go_alone_choice.include?("DOWNTOWN")
     puts "You see the parade downtown, and sneak onto one of the parade floats. Good times! You have an awesome day off!"
+    break
   elsif go_alone_choice == "B" || go_alone_choice.include?("ART") || go_alone_choice.include?("MUSEUM")
     puts "You visit the art museum, and see some inspiring pieces of art. A life changing experience! You have an awesome day off!"
+    break
   elsif go_alone_choice == "C" || go_alone_choice.include?("FANCY")
     puts "You go to a fancy restaurant, and eat some of the finest lobster you've ever eaten. Delicious! You have an awesome day off!"
+    break
   elsif go_alone_choice == "D" || go_alone_choice.include?("SEARS") || go_alone_choice.include?("TOWER")
     puts "You messed up when you got there.  Principal Rooney is also at Sears Tower.  He caught you!  Game over"
+    break
+  else
+    puts "Input valid phrase"
+    end
   end
 end
 
-start(start_choice)
+# start
